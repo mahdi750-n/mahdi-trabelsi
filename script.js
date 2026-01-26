03d47ef8794ad713bea27630c601c78b18c656d6
mahdi750-n <mahditrabelsi140@gmail.com>
2026-01-26 20:50:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -44,5 +44,17 @@ if (navigator.webdriver) {
 if (/bot|crawl|spider|scanner/i.test(navigator.userAgent)) {
   document.body.innerHTML = "";
 }
+document.addEventListener('contextmenu', e => e.preventDefault());
+
+document.addEventListener('keydown', e => {
+  if (
+    e.key === 'F12' ||
+    (e.ctrlKey && e.shiftKey && ['I','J','C'].includes(e.key)) ||
+    (e.ctrlKey && e.key === 'U')
+  ) {
+    e.preventDefault();
+  }
+});
+
 
 
